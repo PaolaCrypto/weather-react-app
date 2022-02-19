bf825b69e2e8a090a0f9d1321e355689bf99cd23
PaolaCrypto <paolaseverino@protonmail.com>
2022-02-19 00:14:05 -0500

    adding bootstrap and link in footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./styles.css";
 import SearchEngine from "./SearchEngine";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Weather Search Engine</h1>
-      <SearchEngine />
-      <footer>
-        This project was coded by Paola Severino and is
-        <a
-          href="https://github.com/PaolaCrypto/weather-react-app"
-          target="_blank"
-        >
-          open sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Hello Weather Search Engine</h1>
+        <SearchEngine />
+        <footer>
+          This project was coded by Paola Severino and is
+          <a
+            href="https://github.com/PaolaCrypto/weather-react-app"
+            target="_blank"
+          >
+            open sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import "bootstrap/dist/css/bootstrap.css";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
